c52f736429de4455408813ef10fbfa476215e533
colorlight <colorlightleecaa@gmail.com>
2017-04-29 22:59:18 +0800

    遍历五颜六色的小框框

---
+++ pratice22/src/test.js
@@ -0,0 +1,5 @@
+var buttonSet =  document.getElementsByClassName('button-set')[0];
+var preButton = buttonSet.firstElementChild,
+	midButton = preButton.nextElementSibling,
+	lastButton = buttonSet.lastElementChild;
+
